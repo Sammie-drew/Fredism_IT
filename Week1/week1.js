ab662c0b3076f33d6025590885f29bc57c480936
Sammie-drew <empireandz@gmail.com>
2020-08-31 19:21:35 +0100

    update

---
+++ Week1/week1.js
@@ -0,0 +1,63 @@
+/** @format */
+
+// let name = "andrew";
+// console.log(typeof name);
+// name = 3;
+// console.log(typeof name);
+
+//Objects
+
+// let person = {
+//   name: "Sammie",
+//   age: "21",
+// };
+
+// console.log(person);
+
+// //bracket notation for objects
+
+// person["name"] = "Drew";
+
+// console.log(person.name);
+
+// Arrays
+
+// let selectedColors = ["green", "blue"];
+// console.log(selectedColors);
+// console.log(typeof selectedColors);
+// selectedColors[2] = "brown";
+
+// selectedColors[3] = 1;
+
+// console.log(selectedColors);
+
+// function greet(name, lastName) {
+//   console.log("Hello " + name + " " + lastName);
+//   return "Happy boy";
+// }
+
+// adding a console,log to a function that is already cl will return undefined as far as no return statement is specified
+// // Drew is an argument to the greet function while name is a parameter of the greet function
+// console.log(greet("sammie", "drew"));
+
+// function square(number) {
+//   return number * number;
+// }
+
+// console.log(square(9));
+
+// operators
+
+// let x = 20;
+// let y = 33;
+// console.log(x + y);
+// console.log(x);
+// console.log(--x);
+// console.log(x++);
+// console.log(x);
+
+//ternary operator
+// let customer = 99;
+
+// customer === 100 ? console.log("happy boy") : console.log("fuck you");
+// false || 1 || 2; it returns 1
