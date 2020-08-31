f9353c9ef6f26d8e95eb956d696997456dc39dc0
Sammie-drew <empireandz@gmail.com>
2020-08-31 23:27:01 +0100

    update

---
+++ Month1/Week4.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Week4 = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>Hello world</Text>
+    </View>
+  );
+};
+
+export default Week4;
